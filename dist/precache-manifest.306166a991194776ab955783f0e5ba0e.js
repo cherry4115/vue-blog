f245afa833a01abe917bd6cc44d17f38af2ee731
wenshuangyun <shuangyun.wen@ele.me>
2018-07-09 17:19:22 +0800

    add dist

---
+++ dist/precache-manifest.306166a991194776ab955783f0e5ba0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c34716eececde712e9",
+    "url": "/js/chunk-vendors.77c34716.js"
+  },
+  {
+    "revision": "ca78bf7f1f27123f5175",
+    "url": "/js/app.ca78bf7f.js"
+  },
+  {
+    "revision": "967b7bdbb06dfd5ab501433408859687",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ca78bf7f1f27123f5175",
+    "url": "/css/app.81b65871.css"
+  }
+];
