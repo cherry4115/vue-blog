8a93eae661275a8126be21ebb5eee4fd41ea6df6
wenshuangyun <shuangyun.wen@ele.me>
2018-07-22 17:48:34 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import Antd from 'vue-antd-ui';
+import 'vue-antd-ui/dist/antd.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(Antd);
 
 new Vue({
   router,
---
+++ src/views/WenSY.vue
@@ -0,0 +1,15 @@
+<template>
+  <blog-home ref="BlogHome"></blog-home>
+</template>
+
+<script>
+import BlogHome from '../components/BlogHome.vue'
+
+export default {
+  name: 'WenSY',
+
+  components: {
+    BlogHome
+  }
+};
+</script>
